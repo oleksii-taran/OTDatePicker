bf839daa417896d68b0c3492740ceb9937a821d5
Oleksii Taran <oleksii.taran@gmail.com>
2014-01-16 23:23:20 +0200

    Add OTCalendarView class stub

---
+++ CalendarView/Sources/Classes/OTCalendarView.h
@@ -0,0 +1,19 @@
+//
+//  OTCalendarView.h
+//  CalendarView
+//
+//  Created by Oleksii Taran on 1/16/14.
+//  Copyright (c) 2014 Oleksii Taran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/// OTCalendarView public interface
+@interface OTCalendarView : UIControl <NSCoding>
+
+@end
+
+/// OTCalendarView protected interface
+@interface OTCalendarView ()
+
+@end
---
+++ CalendarView/Sources/Classes/OTCalendarView.m
@@ -0,0 +1,19 @@
+//
+//  OTCalendarView.m
+//  CalendarView
+//
+//  Created by Oleksii Taran on 1/16/14.
+//  Copyright (c) 2014 Oleksii Taran. All rights reserved.
+//
+
+#import "OTCalendarView.h"
+
+/// OTCalendarView private interface
+@interface OTCalendarView ()
+
+@end
+
+/// OTCalendarView implementation
+@implementation OTCalendarView
+
+@end
